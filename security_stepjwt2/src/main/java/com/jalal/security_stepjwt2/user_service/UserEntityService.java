7de4f34d2c204ff47983c7d8e16da8eae312c83f
motalebjalal <motalebhb@gmail.com>
2024-05-21 20:27:43 +0600

    remove this files

--- security_stepjwt2/src/main/java/com/jalal/security_stepjwt2/user_service/UserEntityService.java
+++
@@ -1,8 +0,0 @@
-package com.jalal.security_stepjwt2.user_service;
-
-import com.jalal.security_stepjwt2.entity.UserEntity;
-
-public interface UserEntityService {
-    public String saveUser(UserEntity user);
-
-}
